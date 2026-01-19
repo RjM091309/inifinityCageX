9e24af0025fe8abc0273509bea02b7aaa0a7208f
romarmaniago <romar.maniago17@gmail.com>
2026-01-19 11:31:53 +0800

    123

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,9 +4,9 @@ module.exports = {
         name: "InfinityCageX",
         script: "app.js", 
         env: {
-          DB_HOST: "127.0.0.1",
-          DB_USER: "3core",
-          DB_PASSWORD: "2024.3core21",
+          DB_HOST: "localhost",
+          DB_USER: "root",
+          DB_PASSWORD: "",
           DB_NAME: "infinitycage",
           DB_PORT: 3306,
           REDIS_HOST: "127.0.0.1",
