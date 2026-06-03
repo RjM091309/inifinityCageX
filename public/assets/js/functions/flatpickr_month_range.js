416db68280b2761e055bb1b136001e55f0e22da3
romarmaniago <romar.maniago17@gmail.com>
2026-06-03 16:51:37 +0800

    Update net profit

--- public/assets/js/functions/flatpickr_month_range.js
+++ public/assets/js/functions/flatpickr_month_range.js
@@ -1,5 +1,6 @@
 /**
  * Click a month name in a multi-month flatpickr range calendar to select that full month.
+ * Auto-applies to all flatpickr instances in range mode.
  */
 (function (global) {
 	function bindFlatpickrMonthNameRangeSelect(instance) {
@@ -61,8 +62,56 @@
 				}
 			}
 			instance.setDate([start, end], true);
+			instance.close();
 		});
 	}
 
+	function patchFlatpickrConfig(config) {
+		config = config || {};
+		if (config.mode !== 'range') {
+			return config;
+		}
+		var userOnReady = config.onReady;
+		var userOnOpen = config.onOpen;
+		return Object.assign({}, config, {
+			onReady: function (selectedDates, dateStr, instance) {
+				bindFlatpickrMonthNameRangeSelect(instance);
+				if (typeof userOnReady === 'function') {
+					userOnReady.call(this, selectedDates, dateStr, instance);
+				}
+			},
+			onOpen: function (selectedDates, dateStr, instance) {
+				if (typeof userOnOpen === 'function') {
+					userOnOpen.call(this, selectedDates, dateStr, instance);
+				}
+				bindFlatpickrMonthNameRangeSelect(instance);
+			}
+		});
+	}
+
+	function installFlatpickrMonthRangeHook() {
+		var fp = global.flatpickr;
+		if (!fp || fp.__fpMonthRangeHook) {
+			return !!fp;
+		}
+		var original = fp;
+		function wrappedFlatpickr(selector, config) {
+			return original(selector, patchFlatpickrConfig(config));
+		}
+		Object.keys(original).forEach(function (key) {
+			wrappedFlatpickr[key] = original[key];
+		});
+		wrappedFlatpickr.__fpMonthRangeHook = true;
+		global.flatpickr = wrappedFlatpickr;
+		return true;
+	}
+
 	global.bindFlatpickrMonthNameRangeSelect = bindFlatpickrMonthNameRangeSelect;
+
+	installFlatpickrMonthRangeHook();
+	if (typeof document !== 'undefined') {
+		document.addEventListener('DOMContentLoaded', installFlatpickrMonthRangeHook);
+		setTimeout(installFlatpickrMonthRangeHook, 0);
+		setTimeout(installFlatpickrMonthRangeHook, 500);
+	}
 })(typeof window !== 'undefined' ? window : this);
